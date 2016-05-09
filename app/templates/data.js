c134ded98590a439362a2fce014818312b02f6cd
Nkelliny <sitelegion@coolsite.net>
2016-05-09 10:21:23 -0400

    Add files via upload

---
+++ app/templates/data.js
@@ -0,0 +1,22 @@
+App.DATA= [
+  {
+    name: "Data",
+    id: "data",
+    messages: [
+      {
+        id: 1,
+        item: "Test item 1",
+        sender: "user1",
+        recipient: "user2",
+        date: new Date(),
+        data: "User.item"
+      }, {
+        id: 2,
+        item: "Test item 2",
+        sender: "user1",
+        recipient: "user2",
+        date: new Date(),
+        data: "User.item"
+      }
+    ]
+  };
